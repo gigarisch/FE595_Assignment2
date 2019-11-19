3ecb1ffc14570d7d41dda2c8ad8150c34e33a1a4
gigarisch <gigarisch@gmail.com>
2019-11-19 09:41:30 -0500

    Fixes for resubmit:
    - added __name__ = '__main__'
    - using relative references for writing output
    - reran output

--- theyfightcrime.py
+++ theyfightcrime.py
@@ -9,39 +9,40 @@ Created for FE595 Assignment 2
 from requests import get # import for curl
 import re # regex package
 import time # sleep function to delay curl requests
-import os # change working directory
 
-# Set working directory.
-wd = "C:\\Users\\gordon.garisch\\Documents\\Projects\\Stevens\\fe595\\FE595_Assignment2"
-os.chdir(wd)
 
-# Open files for writing
-m= open("hes.txt","w+")
-f= open("shes.txt","w+")
+def theyfightcrime():
 
-# Do this 50 times
-for i in range(50):
-    # Retrieve HTML from website
-    text = get("http://www.theyfightcrime.org").text
+    # Open files for writing
+    m= open("hes.txt","w+")
+    f= open("shes.txt","w+")
     
-    # Set up regexp object
-    # Text for male and female superheroes is found between 
-    # <P> and ". They fight crime!"
-    r = re.compile("(<P>(.+?). They fight crime!)")
+    # Do this 50 times
+    for i in range(50):
+        # Retrieve HTML from website
+        text = get("http://www.theyfightcrime.org").text
+        
+        # Set up regexp object
+        # Text for male and female superheroes is found between 
+        # <P> and ". They fight crime!"
+        r = re.compile("(<P>(.+?). They fight crime!)")
+        
+        # Execute regexp search to retrieve test
+        strings = r.findall(text)
+        
+        # Write to files
+        # She must be added back as this is what is split on.
+        # Splitting on She is required since the period between 
+        # the two sentences is often ommitted.
+        m .write("{}. {}\n".format(i+1,strings[0][1].split(" She")[0]))
+        f.write("{}. {}{}\n".format(i+1,"She",strings[0][1].split(" She")[1]))
+        
+        # Wait a while
+        time.sleep(0.05)
     
-    # Execute regexp search to retrieve test
-    strings = r.findall(text)
-    
-    # Write to files
-    # She must be added back as this is what is split on.
-    # Splitting on She is required since the period between 
-    # the two sentences is often ommitted.
-    m .write("{}. {}\n".format(i+1,strings[0][1].split(" She")[0]))
-    f.write("{}. {}{}\n".format(i+1,"She",strings[0][1].split(" She")[1]))
-    
-    # Wait a while
-    time.sleep(0.05)
+    # Close the text files    
+    m.close()
+    f.close()
 
-# Close the text files    
-m.close()
-f.close()
+if __name__ == "__main__":
+    theyfightcrime()
